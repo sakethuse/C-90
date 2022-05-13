790e1e2b2eb42b9d8e1e7dc885f2729232e129e6
sakethuse <86656550+sakethuse@users.noreply.github.com>
2022-05-13 18:47:57 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -24,7 +24,41 @@ function send(){
    console.log(r3)
    tag = "<h3 id='word_display'>Q." + r3 + "</h3>"
    inputtag = "<br>anwser: <input id='input_box' type = 'text'>";
-   buttontag = "<br><button onclick='check()'>check answer</button>"
+   buttontag = "<br><button onclick='check()' class='btn btn-success'>check answer</button>"
    document.getElementById("output").innerHTML = tag+ inputtag + buttontag
    document.getElementById("word").value = ""
+}
+questionturn = "player1"
+ansturn = "player2"
+function check(){
+   getans = document.getElementById("input_box").value;
+   ans = getans.toLowerCase();
+   if(word == ans){
+      if(ansturn == "player1"){
+         player1_score = player1_score + 1;
+         document.getElementById("player1_score").innerHTML = player1_score;
+      }
+      else{
+         player2_score = player2_score + 1;
+         document.getElementById("player2_score").innerHTML = player2_score;
+      }
+   } 
+   if(questionturn == "player1"){
+      questionturn = "player2";
+      document.getElementById("player_question").innerHTML = "question turn:" + player2_name;
+   }
+   else{
+      questionturn = "player1";
+      document.getElementById("player_question").innerHTML = "question turn:" + player1_name;
+   }
+
+   if(ansturn == "player1"){
+      ansturn = "player2";
+      document.getElementById("player_answer").innerHTML = "answer turn:" + player2_name;
+   }
+   else{
+      ansturn = "player1";
+      document.getElementById("player_answer").innerHTML = "answer turn:" + player1_name;
+   }
+   document.getElementById("output").innerHTML = "";
 }
